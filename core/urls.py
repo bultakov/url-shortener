fb3d0a8fb48c42f36f8ef3f5ca3a99b556a78fae
unknown <bii23.uz@gmail.com>
2023-08-22 12:17:27 +0500

    Link Shortener

--- core/urls.py
+++ core/urls.py
@@ -19,10 +19,30 @@ from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
 
+from rest_framework.permissions import AllowAny
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+
+schema_view = get_schema_view(
+    openapi.Info(
+        title="API v1",
+        default_version='v1',
+        description="API description",
+        # terms_of_service="https://www.google.com/policies/terms/",
+        contact=openapi.Contact(name="Ibrohim Bultakov", url="https://bultakov.uz"),
+        # license=openapi.License(name="BSD License"),
+    ),
+    public=True,
+    permission_classes=[AllowAny],
+)
+
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("", include('shortener.urls')),
     path("api/", include('api.urls')),
+
+    path('api/docs/', schema_view.with_ui('swagger', cache_timeout=0), name='docs'),
+    path('api/redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='redoc')
 ]
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
